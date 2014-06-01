86b4a9bab34274d7f524b316964eec4e9d2e6909
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-01 17:07:33 +1000

    Implementing sync logic

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 /* jshint node: true */
 'use strict';
 
+var async = require('async');
+var attachmate = require('attachmate');
+var debug = require('debug')('steelmesh-appsync');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var path = require('path');
+var semver = require('semver');
+
 /**
   # steelmesh-appsync
 
@@ -13,4 +21,72 @@
 **/
 
 module.exports = function(db, opts) {
+  var baseUrl = db.config.url + '/' + db.config.db;
+  var targetPath = (opts || {}).targetPath || process.cwd();
+
+  function compareAndSync(appFolder, pkg, app, callback) {
+    var version = (pkg || {}).version;
+
+    db.get(app.id, function(err, body) {
+      if (err) {
+        return callback(err);
+      }
+
+      // if we have a version and the version not greater than the local
+      // version do nothing (but don't error)
+      if (version && (! semver.gt(body.version, version))) {
+        return callback();
+      }
+
+      mkdirp(appFolder, function(err) {
+        if (err) {
+          return callback(err);
+        }
+
+        attachmate.download(baseUrl + '/' + app.id, appFolder, callback);
+      });
+    });
+  }
+
+  function getPackageData(appFolder, callback) {
+    var pkg;
+
+    fs.exists(appFolder, function(exists) {
+      if (! exists) {
+        return callback();
+      }
+
+      try {
+        pkg = require(path.resolve(appFolder, 'package.json'));
+      }
+      catch (e) {
+        return callback(new Error('Cannot sync - package.json missing or invalid in ' + appFolder));
+      }
+
+      callback(null, pkg);
+    });
+  }
+
+  function sync(app, callback) {
+    var appFolder = path.resolve(targetPath, app.id);
+
+    getPackageData(appFolder, function(err, pkg) {
+      if (err) {
+        return callback(err);
+      }
+
+      compareAndSync(appFolder, pkg, app, callback);
+    });
+  }
+
+  return function(callback) {
+    // list the items in the db
+    db.list(function(err, body) {
+      if (err) {
+        return callback(err);
+      }
+
+      async.map(body.rows || [], sync, callback);
+    });
+  };
 };
